4760c9d61fe2de0f6268ab73e7794b92a320577a
abdelrhamane <abdelrhamane@hotmail.fr>
2015-09-23 08:22:13 +0200

    add : Result Table

---
+++ js/init.js
@@ -0,0 +1,37 @@
+ var mode_result = false;
+
+ // show search form on click btnDisplayFrom
+$('#btnDisplayForm').click(function (){
+    $('#formRecherche').show(300);
+    $('#btnDisplayForm').hide();
+});
+
+// show activity when category choise
+$('#category').change(function(){
+    var list = ['(all)','jetSki', 'truc' , 'muche'];
+    $.each(list, function (id,item) {
+        $('#activity').append($('<option>', {text : item}));
+    });
+});
+
+// Button research / display result and reduce search bar
+$('#btnSearch').click(function() {
+    if(mode_result == false) {
+        $('#top').css('height','70%');
+    }
+    mode_result = true;
+    $('#results').show();
+    window.location.hash = "results";
+});
+
+// Slider de la durée
+$('.nstSlider').nstSlider({ "left_grip_selector": ".leftGrip",
+    "value_changed_callback": function(cause, leftValue, rightValue) { 
+        if(leftValue == 0) {
+             $(this).parent().find('.leftLabel').text(30 + " Mins");
+        }
+        else {
+             $(this).parent().find('.leftLabel').text(leftValue + " Heures");
+        }
+    }   
+});
