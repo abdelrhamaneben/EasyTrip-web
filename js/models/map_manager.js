b18512e183871c2813ee3a55f6b4404f659144e0
abdelrhamane <abdelrhamane@hotmail.fr>
2015-11-02 22:39:39 +0100

    add: function js calcule le rectangle de coordonnée selon le zoom et le centre de la map

--- js/models/map_manager.js
+++ js/models/map_manager.js
@@ -2,6 +2,30 @@
 * This object manage the google map on the view
 */
 var map_manager = {
+	matrice : {
+	21 : { lat :0.001651    , lng:0.0039855 },
+	20 : { lat :0.003302    , lng:0.007971 },
+	19 : { lat :0.006604    , lng:0.015942 },
+	18 : { lat :0.013208    , lng:0.031884 },
+	17 : { lat :0.026416    , lng:0.063768 },
+	16 : { lat :0.052832    , lng:0.127536 },
+	15 : { lat :0.105664    , lng:0.255072 },
+	14 : { lat :0.211328    , lng:0.510144 },
+	13 : { lat :0.422656    , lng:1.020288 },
+	12 : { lat :0.845312    , lng:2.040576 },
+	11 : { lat :1.690624    , lng:4.081152 },
+	10 : { lat :3.381248    , lng:8.162304 },
+	9  : { lat :6.762496    , lng:16.324608 },
+	8  : { lat :13.524992   , lng:32.649216 },
+	7  : { lat :27.049984   , lng:65.298432 },
+	6  : { lat :54.099968   , lng:130.596864 },
+	5  : { lat :108.199936  , lng:261.193728 },
+	4  : { lat :216.399872  , lng:522.387456 },
+	3  : { lat :432.799744  , lng:1044.774912 },
+	2  : { lat :865.599488  , lng:2089.549824 },
+	1  : { lat :1731.198976 , lng:4179.099648 },
+	0  : { lat :3462.397952 , lng:8358.199296 }
+	},
 	// PARAMETERS
 	markers : [],
 	map : null,
@@ -13,8 +37,7 @@ var map_manager = {
         	center: new google.maps.LatLng(longitude, latitude),
         	mapTypeId: google.maps.MapTypeId.ROADMAP
     	});
-
-    	console.log(this.map.getZoom());
+		this.getRectangle(this.map.getZoom(),this.map.getCenter());
 	},
 	// this function add a marker to google map
 	addMarker: function (service) {
@@ -48,14 +71,17 @@ var map_manager = {
 	},
 
 	getRectangle : function (zoom, coordinate) {
-
-		/*
-		var rectangle = "{
-						topright : { lng : , lat : },
-						topleft : { lng : , lat : },
-						bottomright : { lng : , lat : },
-						bottomleft : { lng : , lat : },
-					}";*/
+		var ecartLat = this.matrice[zoom].lat;
+		var ecartLng = this.matrice[zoom].lng;
+		var centerLat = coordinate.lat();
+		var centerLng = coordinate.lng();
+		var rectangle = {
+						topright : { lng : centerLng + ecartLng, lat : centerLat + ecartLat},
+						topleft : { lng : centerLng - ecartLng, lat : centerLat + ecartLat},
+						bottomright : { lng : centerLng + ecartLng, lat : centerLat - ecartLat},
+						bottomleft : { lng : centerLng - ecartLng, lat : centerLat - ecartLat}
+					};
+		console.log(rectangle);
 	},
 	// to define, call to add action on click on marker
 	clickOnMarker : function (service) { }
