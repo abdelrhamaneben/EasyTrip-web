ec8f78acc93781a464bc5a1b709959e7656e64b0
abdelrhamane <abdelrhamane@hotmail.fr>
2015-11-03 11:12:45 +0100

    fix : css

--- js/JsonCategory.js
+++ js/JsonCategory.js
@@ -43,6 +43,45 @@ var categoryJson = {
         {
 			img: "images/4.jpg",
 			name: "Kids"
+		},
+        {
+			img: "images/6.jpg",
+			name: "Night"
+		},
+        {
+			img: "images/7.jpg",
+			name: "Music"
+		},
+        {
+			img: "images/10.jpg",
+			name: "Sports"
+		},
+        {
+			img: "images/2.jpg",
+			name: "Exploration"
+		},
+        {
+			img: "images/11.jpg",
+			name: "Theme park"
+		},{
+			img: "images/1.jpg",
+			name: "Culture"			
+		},
+		{
+			img: "images/3.jpg",
+			name: "gastronomy"
+		},
+		{
+			img: "images/8.jpg",
+			name: "Beach"
+		},
+        {
+			img: "images/6.jpg",
+			name: "Night"
+		},
+        {
+			img: "images/7.jpg",
+			name: "Music"
 		}
 	]
 }
